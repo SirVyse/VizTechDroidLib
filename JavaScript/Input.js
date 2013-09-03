0e2e9b492bb21d306941a07d1f16daf0eb046fba
SirVyse <martin@projectcoin.co.uk>
2013-09-03 12:25:36 +0100

    Debug Commit

--- JavaScript/Input.js
+++ JavaScript/Input.js
@@ -58,9 +58,9 @@
 						height /= 2.0;		
 						
 						Private.m_posX = event.touches[0].clientX - width;
-						Private.m_posX *= window.innerWidth / Private.m_touchAreaWidth;
+						Private.m_posX *= window.innerWidth / TheEngine.Instance().GetScreenWidth();
 						Private.m_posY = event.touches[0].clientY - height;
-						Private.m_posY *= window.innerHeight / Private.m_touchAreaHeight;
+						Private.m_posY *= window.innerHeight / TheEngine.Instance().GetScreenHeight();
 						
 						alert("X:" + Private.m_posX + "\nY:" + Private.m_posY);
 					});
